726ceebbf848b03263c2cd02f91128b7352bbfaf
Develforever <rockfade@gmail.com>
2024-09-22 12:56:00 +0200

    index c onstroller

---
+++ src/Controller/IndexConrtollerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexConrtollerController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('index_conrtoller/index.html.twig', [
+            'controller_name' => 'IndexConrtollerController',
+        ]);
+    }
+}
