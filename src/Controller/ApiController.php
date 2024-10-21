1689d87a7e3f6b0b2b0183833be9c3029b8d61c5
Develforever <rockfade@gmail.com>
2024-10-21 23:04:26 +0200

    custom swagger header with commit hash

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,10 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use OpenApi\Attributes as OA;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api/data', name: 'api_data')]
+    #[Route('/api/data', name: 'api_data', methods: ['GET'])]
+    #[OA\Get(
+        summary: "Secured endpoint",
+        description: "This is a secured endpoint that requires a Bearer token",
+        security: [["Bearer" => []]],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Secured response",
+                content: new OA\JsonContent(type: "object", example: ["data" => "secured data"])
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Unauthorized"
+            )
+        ]
+    )]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function getData(): JsonResponse
     {
--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
 
 composer install
+
+chash=$(git rev-parse HEAD)
+sed -i "/GIT_COMMIT_HASH/c\GIT_COMMIT_HASH=${chash}" .env.local
+
 docker compose up -d --build
