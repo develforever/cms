9ac237e9d5c6e9dda9608bc38f62915ec4afc15d
Develforever <rockfade@gmail.com>
2024-10-28 00:02:15 +0100

    rm assets package; update start script

--- start.sh
+++ start.sh
@@ -3,6 +3,13 @@
 composer install
 
 chash=$(git rev-parse HEAD)
+
+
+if [ ! -f .env.local ]; then
+    echo ".env.local file created"
+    cp .env.local.disy .env.local
+fi
+
 sed -i "/GIT_COMMIT_HASH/c\GIT_COMMIT_HASH=${chash}" .env.local
 
 docker compose up -d --build
